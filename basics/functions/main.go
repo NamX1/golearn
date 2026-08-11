d8837aa6851cbc78630f5f053fdb644cd80fe2db
NamX1 <kc8307799@gmail.com>
2026-08-11 21:36:30 +0700

    Reorganize basics folder and fix module path
    Rename basics/helloworld to basics/hello-world, add functions
    example, remove now-unused http/client and http/server (moved to
    networking/), and correct go.mod module path to match the GitHub repo.

---
+++ basics/functions/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func welcomeMessage(name string) {
+	fmt.Printf("Welcome %s, into our library!", name)
+}
+
+func main() {
+	welcomeMessage("NamX1")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module github.com/NamX1/golearn
 
 go 1.26.5
